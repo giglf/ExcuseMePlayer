648274df2f71b93c0cab18beccd66557536d88ea
giglf <lin.giglf@gmail.com>
2017-04-21 10:32:35 +0800

    Commit two music info model.

---
+++ src/app/excuseme/model/MusicInfo.java
@@ -0,0 +1,98 @@
+package app.excuseme.model;
+
+/**
+ * 
+ * 封装的单首歌曲的信息</br>
+ * 包含歌曲基本属性的getter和setter</br>
+ * <p><b><pre>
+ * album			专辑</br>
+ * artist			艺术家</br>
+ * audio			音频URL</br>
+ * company		唱片公司</br>
+ * cover			专辑封面URL</br>
+ * kbps			码率</br>
+ * key			歌曲key</br>
+ * public_time		出版年份</br>
+ * title			歌曲名</br>
+ * upload_date		上传时间
+ * </pre></b></p>
+ * 
+ */
+public class MusicInfo {
+
+	private String album;			//专辑
+	private String artist;			//艺术家
+	private String audio;			//音频URL
+	private String company;			//唱片公司
+	private String cover;			//专辑封面URL
+	private String kbps;			//码率
+	private String key;				//歌曲key
+	private String public_time;		//出版年份
+	private String title;			//歌曲名
+	private String upload_date;		//上传时间
+	
+	
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getAudio() {
+		return audio;
+	}
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getCover() {
+		return cover;
+	}
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+	public String getKbps() {
+		return kbps;
+	}
+	public void setKbps(String kbps) {
+		this.kbps = kbps;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getPublicTime() {
+		return public_time;
+	}
+	public void setPublicTime(String public_time) {
+		this.public_time = public_time;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUploadDate() {
+		return upload_date;
+	}
+	public void setUploadDate(String upload_date) {
+		this.upload_date = upload_date;
+	}
+	
+	
+	
+}
---
+++ src/app/excuseme/model/PlayListInfo.java
@@ -0,0 +1,41 @@
+package app.excuseme.model;
+
+/**
+ * Doufm的api中包含了11个频道</br>
+ * 此处对这些频道的信息进行封装</br>
+ * 一个电台频道包含key, music_list, name三个属性，该类中包含相应的setter和getter</br>
+ * <p><b><pre>
+ * key			该电台频道的key</br>
+ * music_list		该电台频道中的歌曲总数</br>
+ * name			该电台频道的名称</br>
+ * </pre></b></p>
+ */
+public class PlayListInfo {
+
+	private String key;				//该电台频道的key
+	private String music_list;		//该电台频道中的歌曲总数
+	private String name;			//该电台频道的名称
+	
+	
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getMusiclist() {
+		return music_list;
+	}
+	public void setMusiclist(String music_list) {
+		this.music_list = music_list;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
