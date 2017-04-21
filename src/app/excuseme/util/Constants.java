876f6cc6890f12ee0cd4a599bf469b37bcd95d7d
giglf <lin.giglf@gmail.com>
2017-04-21 10:36:23 +0800

    Add a constants helper class.

---
+++ src/app/excuseme/util/Constants.java
@@ -0,0 +1,21 @@
+package app.excuseme.util;
+
+
+/**
+ * 储存全局常量的类</br>
+ * 会根据需要随时更新
+ */
+public class Constants {
+
+	public static final String BASE_URL = "http://doufm.info";
+    public static final String PLAYLIST_URL = BASE_URL + "/api/playlist/";
+    public static final String MUSIC_IN_PLAYLIST_URL = BASE_URL + "/api/playlist/";
+//    public static final String LOGIN_URL = BASE_URL + "/api/app_auth/";
+    public static final String MUSIC_URL = BASE_URL + "/api/music/";
+//    public static final String CHANNEL_URL = BASE_URL + "/api/channel/";
+//    public static final String LOGOUT_URL = BASE_URL + "/api/user/logout/";
+//    public static final String USER_PROFILE_URL = BASE_URL + "/api/user/profile/";
+//    public static final String USER_MUSIC_URL = BASE_URL + "/api/user/music/";
+//    public static final String USER_HISTORY_URL = BASE_URL + "/api/user/history/";
+	
+}
