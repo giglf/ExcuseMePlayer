85a5b369a1bbb6830b2d31b4b68ac2e8bb8945b1
giglf <lin.giglf@gmail.com>
2017-05-07 10:07:23 +0800

    Add some resources.

--- src/app/excuseme/util/Constants.java
+++ src/app/excuseme/util/Constants.java
@@ -5,8 +5,10 @@ package app.excuseme.util;
  * 储存全局常量的类</br>
  * 会根据需要随时更新
  */
-public class Constants {
+public final class Constants {
 
+	
+	//doufm api url属性常量
 	public static final String BASE_URL = "http://doufm.info";
     public static final String PLAYLIST_URL = BASE_URL + "/api/playlist/";
     public static final String MUSIC_IN_PLAYLIST_URL = BASE_URL + "/api/playlist/";
@@ -18,4 +20,12 @@ public class Constants {
 //    public static final String USER_MUSIC_URL = BASE_URL + "/api/user/music/";
 //    public static final String USER_HISTORY_URL = BASE_URL + "/api/user/history/";
 	
+    
+    //APP属性常量
+    public static final String APP_TITLE = "Excuse Me???";
+    
+    
+    //资源路径
+    public static final String IMAGE = "/app/excuseme/util/image/";
+    public static final String FXML = "/app/excuseme/view/";
 }
